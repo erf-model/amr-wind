458abff9aae3c581c809205fae2bed2780770527
Mukul Dave <mhdave@lbl.gov>
2024-10-15 16:40:11 -0700

    add back required ifdef

--- amr-wind/wind_energy/ABLBoundaryPlane.cpp
+++ amr-wind/wind_energy/ABLBoundaryPlane.cpp
@@ -1255,6 +1255,7 @@ void ABLBoundaryPlane::read_file(const bool nph_target_time)
         return;
     }
 
+#ifdef ERF_AW_MULTIBLOCK
     if (m_out_fmt == "erf-multiblock") {
         //m_read_erf = sim.get_read_erf();
         ReadERFFcn read_erf = *m_read_erf;
@@ -1266,6 +1267,7 @@ void ABLBoundaryPlane::read_file(const bool nph_target_time)
         }
         return;
     }
+#endif
 
     // populate planes and interpolate
     const amrex::Real time =
